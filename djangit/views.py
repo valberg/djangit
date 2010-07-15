1ad10649e3961ce501cd2b19625cfad3046c614a
Vidir Valberg Gudmundsson <vidir.valberg@gmail.com>
2010-07-16 01:02:06 +0200

    Initial refactoring and documentation done, needs testing though

--- djangit/views.py
+++ djangit/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render_to_response
 from djangit import config
 
-
 import dulwich
 import re, glob, os
 
+
+def seperate_tree_entries(tree_entries, tree_path, repo):
+    """ Seperates tree entries
+    
+    Iterates through the tree entries to place trees in one list and blobs in 
+    another depending on the entrys type_num.
+
+    """
+    trees = []
+    blobs = []
+
+    for mode, name, sha in tree_entries:
+        if tree_path:
+            url = tree_path + '/' + name
+        else:
+            url = name
+        
+        entry = repo.get_object(sha)
+        
+        if entry.type_num == 2:
+            trees.append((mode, name, url, sha))
+        elif entry.type_num == 3:
+            blobs.append((mode, name, url, sha))
+
+    return trees, blobs
+
 def list_repos(request):
-    """ List all repos """
+    """ List all repos 
+    
+    Globs for all directories ending in .git in GIT_REPOS_DIR and then runs a
+    regex on the path of each directory to get only the name of the directory.
+
+    Finally puts each directory in a list, 'repos', which gets sent as context
+    via render_to_response.
+    """
 
     repos = []
 
@@ -16,131 +48,151 @@ def list_repos(request):
 
     return render_to_response('djangit/list_repos.html', {'repos':repos})
 
-def show_repo(request, repo, ref):
-    """ Show info about a repo """
+def show_repo(request, repo_name, ref_name):
+    """ Show info about a repo 
+    
+    Creates a repo object using repo_name, gets the latest commit and
+    corresponding tree and tree entries.
 
-    r = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo + '.git')
+    Procedes onto references of the repo. Currently 'HEAD' isn't used for
+    anything, so this is gently "passed out", and since references are
+    displayed as refs/heads/<name> each reference is split, and only the last
+    part is put into a list, 'refs'.
 
-    commit = r[r.ref('refs/heads/' + ref)]
-    tree = r[commit.tree]
+    All this is then sent into space with render_to_response, incl. repo_name
+    and ref_name which come from the URL.
+    """
+
+    repo = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo_name + '.git')
+
+    # Getting the latest commit.
+    commit = repo[repo.ref('refs/heads/' + ref_name)]
+
+    # Getting the tree of latest commit.
+    tree = repo[commit.tree]
+
+    # Getting entries from the tree.
     tree_entries = tree.entries()
 
-    trees = []
-    blobs = []
-
-    for mode, name, sha in tree_entries:
-        e = r.get_object(sha)
-        if e.type_num == 2:
-            url = name
-            trees.append((mode, name, url, sha))
-        elif e.type_num == 3:
-            url = name
-            blobs.append((mode, name, url, sha))
+    trees, blobs = seperate_tree_entries(tree_entries, tree_path, repo)
 
     # References
-
     refs = []
     
-    for e in r.refs.as_dict():
-        if e == "HEAD":
+    for ref in repo.refs.as_dict():
+        if ref == "HEAD":
+            # Currently we do not use HEAD for anything, might come later.
             pass
         else:
-            m = e.split('/')
-            refs.append(m[-1])
+            # We only want the last in ie. refs/heads/master
+            parts = ref.split('/') 
+            refs.append(parts[-1])
 
     return render_to_response('djangit/show_repo.html', {
-        'repo':repo, 
-        'ref':ref,
-        'commit':commit,
-        'refs':refs,
-        'trees':trees,
-        'blobs':blobs,
+        'repo_name' : repo_name, 
+        'ref_name'  : ref_name,
+        'commit'    : commit,
+        'refs'      : refs,
+        'trees'     : trees,
+        'blobs'     : blobs,
     })
 
-def list_commits(request, repo, ref):
-    """ List all commits for ref """
+def list_commits(request, repo_name, ref_name):
+    """ List all commits for ref
+    
+    Creates a repo objects using repo_name, and gets revision history, that is
+    all commits, of the provided ref_name.
+    """
 
-    r = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo + '.git')
+    repo = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo_name + '.git')
 
-    commits = r.revision_history('refs/heads/' +ref)
+    # Since revision_history wants the whole name of the reference, incl.
+    # the refs/head/ part we need to prepend that to the ref_name.
+    commits = repo.revision_history('refs/heads/' + ref_name)
     
     return render_to_response('djangit/list_commits.html', {
-        'repo':repo,
-        'ref':ref,
-        'commits':commits,
+        'repo_name' : repo_name,
+        'ref_name'  : ref_name,
+        'commits'   : commits,
     })
 
-def show_tree(request, repo, ref, tree):
-    """ Show tree """
-
-    r = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo + '.git')
-
-    if len(ref) == 40:
-        c = r[ref]
-        t = r[c.tree]
-    else:
-        t = r[r['refs/heads/' + ref].tree]
-
-    if tree:
-        for p in tree.split('/'):
-            t = r[t[p][1]]
-
-    tree_entries = t.entries()
+def show_tree(request, repo_name, ref_name, tree_path):
+    """ Show tree 
     
-    trees = []
-    blobs = []
+    Creates a repo object from repo_name, checks ref_name for if it's a sha
+    value or a normal name, and creates tree object accordingly (might cause
+    problems for reference names with 40 characters, but those are unlikely to 
+    exist). Then it finds the corresponding tree entry by iterating till the
+    last part of the path. Runs seperate_tree_entries and in the end
+    render_to_response to show this lovely tree to the world.
+    """
 
-    for mode, name, sha in tree_entries:
-        e = r.get_object(sha)
-        if e.type_num == 2:
-            if tree:
-                url = tree+'/'+name
-            else:
-                url = name
-            trees.append((mode, name, url, sha))
-        elif e.type_num == 3:
-            if tree:
-                url = tree + '/' + name
-            else:
-                url = name
-            blobs.append((mode, name, url, sha))
+    repo = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo_name + '.git')
 
+    # Check if the ref_name is 40 chars, if so it must be a sha
+    if len(ref_name) == 40:
+        commit = repo[ref_name]
+        tree = repo[commit.tree_path]
+    # else it's just a normal reference name.
+    else:
+        tree = repo[repo['refs/heads/' + ref_name].tree]
+
+    
+    if tree_path:
+        for part in tree_path.split('/'):
+            tree = repo[tree[part][1]]
+
+    tree_entries = tree.entries()
+    
+    trees, blobs = seperate_tree_entries(tree_entries, tree_path, repo)
+    
     return render_to_response('djangit/show_tree.html', {
-        'repo': repo,
-        'ref': ref,
-        'tree': tree,
-        'trees': trees,
-        'blobs': blobs,
+        'repo_name' : repo_name,
+        'ref_name'  : ref_name,
+        'tree_path' : tree_path,
+        'trees'     : trees,
+        'blobs'     : blobs,
     })
 
-def show_blob(request, repo, ref, blob):
-    """ Show blob """
+def show_blob(request, repo_name, ref_name, blob):
+    """ Show blob 
+    Creates a repo object, checks if it comes from a normal name or sha value
+    ref_name, finds it's way to the right tree (which actually in the end is a
+    blob, so it is quite misleading that we are calling the variable a tree)
+    and then in the end render_to_response to show the world this blobby blob!
+    """
 
-    r = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo + '.git')
+    repo = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo_name + '.git')
     
-    if len(ref) == 40:
-        c = r[ref]
-        t = r[c.tree]
+    if len(ref_name) == 40:
+        commit = repo[ref_name]
+        tree = repo[commit.tree]
     else:
-        t = r[r['refs/heads/' + ref].tree]
+        tree = repo[repo['refs/heads/' + ref_name].tree]
 
-    for p in blob.split('/'):
-        t = r[t[p][1]]
+    for part in blob.split('/'):
+        tree = repo[tree[part][1]]
     
     return render_to_response('djangit/show_blob.html', { 
-        'repo': repo,
-        'blob': t,
+        'repo': repo_name,
+        'blob': tree,
     })
 
-def show_commit(request, repo, ref, sha):
-    """ Show commit """
+def show_commit(request, repo_name, ref_name, sha):
+    """ Show commit 
     
-    r = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo + '.git')
+    Pretty straight forward:
+    1. create a repo object
+    2. find commit via sha value
+    3. use render_to_response to show the world!
+    """
+    
+    repo = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo_name + '.git')
 
-    commit = r[sha]
+    commit = repo[sha]
 
     return render_to_response('djangit/show_commit.html', {
-        'repo': repo,
-        'ref': ref,
-        'commit': commit,
+        'repo_name' : repo_name,
+        'ref_name'  : ref_name,
+        'commit'    : commit,
     })
