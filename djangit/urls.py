d5a708d99345e434bdd17f212f8d044532b16a92
Vidir Valberg Gudmundsson <vidir.valberg@gmail.com>
2010-07-19 23:56:27 +0200

    Bored - PEP8'd urls.py

--- djangit/urls.py
+++ djangit/urls.py
@@ -2,16 +2,21 @@ from django.conf.urls.defaults import *
 from djangit.views import *
 
 urlpatterns = patterns('',
-    
-    (r'^(?P<repo_name>[^/]*)/commits/(?P<ref_name>[^/]*)/$', list_commits),
-    
-    (r'^(?P<repo_name>[^/]*)/tree/(?P<ref_name>[^/]*)/(?P<tree_path>.*)$', show_tree),
-    
-    (r'^(?P<repo_name>[^/]*)/blob/(?P<ref_name>[^/]*)/(?P<blob_path>.*)$', show_blob),
-    
-    (r'^(?P<repo_name>[^/]*)/commit/(?P<ref_name>[^/]*)/(?P<sha>\w{40})$', show_commit),
-    
-    (r'^(?P<repo_name>[^/]*)/(?P<ref_name>[^/]*)$', show_repo),
-    
+
+    (r'^(?P<repo_name>[^/]*)/commits/(?P<ref_name>[^/]*)/$',
+        list_commits),
+
+    (r'^(?P<repo_name>[^/]*)/tree/(?P<ref_name>[^/]*)/(?P<tree_path>.*)$',
+        show_tree),
+
+    (r'^(?P<repo_name>[^/]*)/blob/(?P<ref_name>[^/]*)/(?P<blob_path>.*)$',
+        show_blob),
+
+    (r'^(?P<repo_name>[^/]*)/commit/(?P<ref_name>[^/]*)/(?P<sha>\w{40})$',
+        show_commit),
+
+    (r'^(?P<repo_name>[^/]*)/(?P<ref_name>[^/]*)$',
+        show_repo),
+
     (r'^$', list_repos),
 )
