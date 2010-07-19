9f6b8c0241e8ec98829f23f1fc853c366f2aa2c7
Vidir Valberg Gudmundsson <vidir.valberg@gmail.com>
2010-07-19 23:53:01 +0200

    PEP8'd views.py

--- djangit/views.py
+++ djangit/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render_to_response
 from djangit import config
 
 import dulwich
-import re, glob, os
+import re
+import glob
+import os
 
 
 def seperate_tree_entries(tree_entries, tree_path, repo):
     """ Seperates tree entries
-    
-    Iterates through the tree entries to place trees in one list and blobs in 
+
+    Iterates through the tree entries to place trees in one list and blobs in
     another depending on the entrys type_num.
 
     """
@@ -20,9 +22,9 @@ def seperate_tree_entries(tree_entries, tree_path, repo):
             url = tree_path + '/' + name
         else:
             url = name
-        
+
         entry = repo.get_object(sha)
-        
+
         if entry.type_num == 2:
             trees.append((mode, name, url, sha))
         elif entry.type_num == 3:
@@ -30,9 +32,10 @@ def seperate_tree_entries(tree_entries, tree_path, repo):
 
     return trees, blobs
 
+
 def list_repos(request):
-    """ List all repos 
-    
+    """ List all repos
+
     Globs for all directories ending in .git in GIT_REPOS_DIR and then runs a
     regex on the path of each directory to get only the name of the directory.
 
@@ -42,15 +45,16 @@ def list_repos(request):
 
     repos = []
 
-    for dir in glob.glob( os.path.join(config.GIT_REPOS_DIR, '*.git') ):
+    for dir in glob.glob(os.path.join(config.GIT_REPOS_DIR, '*.git')):
         repo_name = re.search('(?P<dir>[^/]*)\.git$', dir)
         repos.append(repo_name.group('dir'))
 
-    return render_to_response('djangit/list_repos.html', {'repos':repos})
+    return render_to_response('djangit/list_repos.html', {'repos': repos})
+
 
 def show_repo(request, repo_name, ref_name):
-    """ Show info about a repo 
-    
+    """ Show info about a repo
+
     Creates a repo object using repo_name, gets the latest commit and
     corresponding tree and tree entries.
 
@@ -78,28 +82,29 @@ def show_repo(request, repo_name, ref_name):
 
     # References
     refs = []
-    
+
     for ref in repo.refs.as_dict():
         if ref == "HEAD":
             # Currently we do not use HEAD for anything, might come later.
             pass
         else:
             # We only want the last in ie. refs/heads/master
-            parts = ref.split('/') 
+            parts = ref.split('/')
             refs.append(parts[-1])
 
     return render_to_response('djangit/show_repo.html', {
-        'repo_name' : repo_name, 
-        'ref_name'  : ref_name,
-        'commit'    : commit,
-        'refs'      : refs,
-        'trees'     : trees,
-        'blobs'     : blobs,
+        'repo_name': repo_name,
+        'ref_name': ref_name,
+        'commit': commit,
+        'refs': refs,
+        'trees': trees,
+        'blobs': blobs,
     })
+
 
 def list_commits(request, repo_name, ref_name):
     """ List all commits for ref
-    
+
     Creates a repo objects using repo_name, and gets revision history, that is
     all commits, of the provided ref_name.
     """
@@ -109,19 +114,20 @@ def list_commits(request, repo_name, ref_name):
     # Since revision_history wants the whole name of the reference, incl.
     # the refs/head/ part we need to prepend that to the ref_name.
     commits = repo.revision_history('refs/heads/' + ref_name)
-    
+
     return render_to_response('djangit/list_commits.html', {
-        'repo_name' : repo_name,
-        'ref_name'  : ref_name,
-        'commits'   : commits,
+        'repo_name': repo_name,
+        'ref_name': ref_name,
+        'commits': commits,
     })
 
+
 def show_tree(request, repo_name, ref_name, tree_path):
-    """ Show tree 
-    
+    """ Show tree
+
     Creates a repo object from repo_name, checks ref_name for if it's a sha
     value or a normal name, and creates tree object accordingly (might cause
-    problems for reference names with 40 characters, but those are unlikely to 
+    problems for reference names with 40 characters, but those are unlikely to
     exist). Then it finds the corresponding tree entry by iterating till the
     last part of the path. Runs seperate_tree_entries and in the end
     render_to_response to show this lovely tree to the world.
@@ -137,25 +143,25 @@ def show_tree(request, repo_name, ref_name, tree_path):
     else:
         tree = repo[repo['refs/heads/' + ref_name].tree]
 
-    
     if tree_path:
         for part in tree_path.split('/'):
             tree = repo[tree[part][1]]
 
     tree_entries = tree.entries()
-    
+
     trees, blobs = seperate_tree_entries(tree_entries, tree_path, repo)
-    
+
     return render_to_response('djangit/show_tree.html', {
-        'repo_name' : repo_name,
-        'ref_name'  : ref_name,
-        'tree_path' : tree_path,
-        'trees'     : trees,
-        'blobs'     : blobs,
+        'repo_name': repo_name,
+        'ref_name': ref_name,
+        'tree_path': tree_path,
+        'trees': trees,
+        'blobs': blobs,
     })
 
+
 def show_blob(request, repo_name, ref_name, blob_path):
-    """ Show blob 
+    """ Show blob
     Creates a repo object, checks if it comes from a normal name or sha value
     ref_name, finds it's way to the right tree (which actually in the end is a
     blob, so it is quite misleading that we are calling the variable a tree)
@@ -163,7 +169,7 @@ def show_blob(request, repo_name, ref_name, blob_path):
     """
 
     repo = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo_name + '.git')
-    
+
     if len(ref_name) == 40:
         commit = repo[ref_name]
         tree = repo[commit.tree]
@@ -179,28 +185,28 @@ def show_blob(request, repo_name, ref_name, blob_path):
 
     if md:
         markdown = True
-    
-    return render_to_response('djangit/show_blob.html', { 
+
+    return render_to_response('djangit/show_blob.html', {
         'repo': repo_name,
         'blob': tree,
-        'markdown': markdown
-    })
+        'markdown': markdown, })
+
 
 def show_commit(request, repo_name, ref_name, sha):
-    """ Show commit 
-    
+    """ Show commit
+
     Pretty straight forward:
     1. create a repo object
     2. find commit via sha value
     3. use render_to_response to show the world!
     """
-    
+
     repo = dulwich.repo.Repo(config.GIT_REPOS_DIR + repo_name + '.git')
 
     commit = repo[sha]
 
     return render_to_response('djangit/show_commit.html', {
-        'repo_name' : repo_name,
-        'ref_name'  : ref_name,
-        'commit'    : commit,
+        'repo_name': repo_name,
+        'ref_name': ref_name,
+        'commit': commit,
     })
