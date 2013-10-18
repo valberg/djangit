b359d7369bc8fa3db62c9536c1397c2eec9ec5c7
Víðir Valberg Guðmundsson <vidir.valberg@orn.li>
2013-10-18 22:11:02 +0200

    Added a breadcrumb template tag.

--- djangit/templatetags/djangit_tags.py
+++ djangit/templatetags/djangit_tags.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import string
 
 from django import template
 
@@ -11,6 +12,14 @@ register = template.Library()
 
 @register.inclusion_tag('djangit/includes/commit_info.html')
 def djangit_commit_info(repo, identifier, link_to_tree=False):
+    """
+    Show info about a commit.
+
+    :param repo: A Repository model instance which has the commit.
+    :param identifier: The identifier for the commit.
+    :param link_to_tree: A boolean that defines whether to show a link to the
+        tree of the commit or not.
+    """
 
     repo_object = repo.get_repo_object()
 
@@ -33,9 +42,17 @@ def djangit_commit_info(repo, identifier, link_to_tree=False):
 
 @register.inclusion_tag('djangit/includes/tree.html')
 def djangit_tree(repo, identifier, path=None, show_readme=True):
+    """
+    Show a tree.
+
+
+    :param repo: A Repository model instance which has the tree.
+    :param identifier: The identifier for the tree.
+    :param path: What path to show. If None the root will be shown.
+    :param show_readme: Whether to check for a README file and show it below the tree.
+    """
 
     context = {}
-
     repo_object = repo.get_repo_object()
 
     # Check if the identifier is 40 chars, if so it must be a sha
@@ -67,19 +84,74 @@ def djangit_tree(repo, identifier, path=None, show_readme=True):
     return context
 
 @register.inclusion_tag('djangit/includes/breadcrumb.html')
-def djangit_breadcrumb(repo_name, tree):
+def djangit_breadcrumb(repo, identifier, path=None):
+    """
+    Show a breadcrumb for the current location in the tree.
+
+    :param repo: Which repository.
+    :param tree: The tree to show.
+    :param path: What path to show. If None the root will be shown.
+    """
+
+    repo_object = repo.get_repo_object()
+
+    if len(identifier) == 40:
+        tree = repo_object[identifier]
+    # else it's just a normal reference name.
+    else:
+        tree = repo_object[repo_object['refs/heads/' + identifier].tree]
+
+    context = {
+        'repo': repo,
+        'identifier': identifier,
+    }
+
+    crumbs = []
+
+    path_parts = path.split('/')
+
+    for i in range(len(path_parts)):
+        name = path_parts[i]
+
+        # TODO: This is somewhat hackish.
+        if i == 0:
+            path_template = '{}{}'
+        else:
+            path_template = '{}/{}'
+
+        link_path = path_template.format(string.join(path_parts[:i], '/'), name)
+
+        print link_path
+
+        crumbs.append((name, link_path))
+
+    context['crumbs'] = crumbs
+
+    return context
+
+@register.inclusion_tag('djangit/includes/repo_info.html')
+def djangit_repo_info(repo):
+    """
+    Show general information about a repo.
+
+    :param repo: Model instance for the repo that should be shown.
+    """
     pass
 
-@register.inclusion_tag('djangit/includes/breadcrumb.html')
-def djangit_repo_info(repo_name):
-    pass
+@register.inclusion_tag('djangit/includes/ref_picker.html')
+def djangit_ref_picker(repo):
+    """
+    List all references.
 
-@register.inclusion_tag('djangit/includes/branch_picker.html')
-def djangit_branch_picker(repo_name):
+    :params repo: Model instance for the repo whose references should be shown
+    """
     pass
 
 @register.filter
 def djangit_format_author(author):
+    """
+    Get name and email from a string like this: "Name <email>"
+    """
     if len(author) > 0:
         ms = author.index('<')
         name = author[:ms].strip(' ')
