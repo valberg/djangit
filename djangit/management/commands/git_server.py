66b93564abebd6c0fec957f09a10541a555d67f1
Víðir Valberg Guðmundsson <valberg@orn.li>
2014-12-27 22:39:50 +0100

    Use the right setting

--- djangit/management/commands/git_server.py
+++ djangit/management/commands/git_server.py
@@ -10,6 +10,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         address = '0.0.0.0'
         port = 9418
-        backend = FileSystemBackend(settings.DJANGIT_GIT_REPOS)
+        backend = FileSystemBackend(settings.DJANGIT_REPOS_DIR)
         server = TCPGitServer(backend, address, port)
         server.serve_forever()
